d6c1b92d5a2cabca348652b1756f34cf76d87561
KellyStamps <kellydsample@gmail.com>
2018-02-13 09:36:18 -0500

    added simple login

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::UsersController < ApplicationController
 
+  def create
+    user = User.new(username: params[:username], password_digest: params[:password], bio: params[:bio])
+
+    if user.save
+      render json: user
+    else
+      render json: {error: 'Could not save user Im so sorry'}
+    end
+  end
+
   def index
     render json: {hello: 'hi'}
   end
